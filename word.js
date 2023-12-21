008113d6362cf6a49064ca3ee67bef21dd36a7c4
Max <syst38@gmail.com>
2023-12-21 01:43:25 +0100

    first commit

--- word.js
+++ word.js
@@ -5,14 +5,17 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: 'https://aesthetic-croquembouche-fd6e15.netlify.app',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://aesthetic-croquembouche-fd6e15.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
 app.post('/pobierz-slowa', async (req, res) => {
   const words = req.body.words || [];
   const postData = { words };
