6392d3539e93f40d707d82934baff9e001cb847a
Max <syst38@gmail.com>
2023-12-25 17:55:22 +0100

    first commit

--- index.js
+++ index.js
@@ -28,29 +28,29 @@ const httpProxy = require("http-proxy");
 
 
 function customCors(req, res, next) {
-	// Adres, z którego chcesz zezwolić na żądania (możesz zmienić ten adres)
-	const allowedOrigin = 'https://sprightly-tulumba-2baacf.netlify.app/register';
-  
-	// Sprawdź, czy żądanie pochodzi z dozwolonego adresu
-	const requestOrigin = req.headers.origin;
-	if (requestOrigin === allowedOrigin) {
-	  res.setHeader('Access-Control-Allow-Origin', requestOrigin);
-	  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-	  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-	  // Obsługa żądań OPTIONS (preflight requests)
-	  if (req.method === 'OPTIONS') {
-		res.writeHead(200);
-		res.end();
-		return;
-	  }
-	}
-  
-	next(); // Kontynuuj przetwarzanie żądania
-  }
-  
-  // Użycie naszego własnego middleware CORS
-  app.use(customCors);
+    // Adres, z którego chcesz zezwolić na żądania (możesz zmienić ten adres)
+    const allowedOrigin = 'https://sprightly-tulumba-2baacf.netlify.app';
+
+    // Sprawdź, czy żądanie pochodzi z dozwolonego adresu
+    const requestOrigin = req.headers.origin;
+    if (requestOrigin === allowedOrigin) {
+        res.setHeader('Access-Control-Allow-Origin', requestOrigin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+
+    // Obsługa żądań OPTIONS (preflight requests)
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+    next(); // Kontynuuj przetwarzanie żądania
+}
+
+// Użycie naszego własnego middleware CORS
+app.use(customCors);
 
 
 
