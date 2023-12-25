c50f5f78198cd889b0913079faef7756d8878e4a
Max <syst38@gmail.com>
2023-12-25 22:50:17 +0100

    first commit

--- check.js
+++ check.js
@@ -51,129 +51,6 @@ function customCors(req, res, next) {
 
 // Użycie naszego własnego middleware CORS
 app.use(customCors);
-
-
-
-
-// const allowedOrigin = process.env.REACT_APP_BACKEND_URL
-
-// app.use(cors({
-//   origin: [process.env.REACT_APP_BACKEND_URL],
-// }));
-// app.use(express.json()); // Parsowanie danych jako JSON
-// app.use(helmet()); // Dodanie zabezpieczeń Helmet
-// app.use(
-//   cors({
-//     origin: process.env.REACT_APP_BACKEND_URL,
-//     methods: ['GET', 'POST'], // Dozwolone metody
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Dozwolone nagłówki
-//   })
-// );
-// app.options('/addTask', cors());
-// app.options('/register', cors());
-// app.options('/odbierzDane', cors());
-// app.options('/test', cors());
-
-// app.options("/register", (req, res) => {
-//   res.header("Access-Control-Allow-Origin", allowedOrigin);
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.send();
-// });
-
-// app.options("/odbierzDane", (req, res) => {
-//   res.header("Access-Control-Allow-Origin", allowedOrigin);
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.send();
-// });
-// app.post("/odbierzDane", (req, res) => {
-//   const receivedData = req.body.data;
-//   console.log("Otrzymane dane:", receivedData);
-
-//   const responseMessage = "Dane odebrane pomyślnie";
-//   res.status(200).json({ message: responseMessage });
-// });
-
-// Obsługa proxy dla zewnętrznego zasobu - tu jest poprawka
-// const proxy = httpProxy.createProxyServer();
-// app.use('/proxy', (req, res) => {
-//   proxy.web(req, res, { target: 'http://localhost:3000' });
-// });
-
-// const proxy = httpProxy.createProxyServer();
-// app.options('/register', (req, res) => {
-// 	res.header('Access-Control-Allow-Origin', 'https://sprightly-tulumba-2baacf.netlify.app');
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 	res.send(); // Odpowiedź pusta dla zapytań OPTIONS
-//   });
-// app.use(cors({
-//   origin: 'https://sprightly-tulumba-2baacf.netlify.app',
-//   methods: ['GET', 'POST', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization', 'application/json'],
-
-//   credentials: true,
-// }));
-
-// app.use('/test', (req, res) => {
-//   proxy.web(req, res, { target: 'http:/localhost:3000' });
-// });
-// app.get('/test', (req, res) => {
-// 	const testUrl = 'https://sprightly-tulumba-2baacf.netlify.app';
-
-// 	// Wykonaj żądanie GET za pomocą modułu http
-// 	http.get(testUrl, (response) => {
-// 		let data = '';
-
-// 		response.on('data', (chunk) => {
-// 			data += chunk;
-// 		});
-
-// 		response.on('end', () => {
-// 			console.log('Odpowiedź z serwera testUrl:', data);
-// 			res.send(data); // Odeślij odpowiedź do klienta Express
-// 		});
-// 	}).on('error', (error) => {
-// 		console.error('Błąd podczas połączenia z testUrl:', error);
-// 		res.status(500).send('Błąd podczas pobierania danych'); // Odeślij błąd do klienta Express
-// 	});
-// });
-// app.use(
-// 	'/proxy',
-// 	createProxyMiddleware({
-// 		target: 'https://sprightly-tulumba-2baacf.netlify.app',
-// 		changeOrigin: true,
-// 		pathRewrite: {
-// 			'^/proxy': '',
-// 		},
-// 		secure: false, // Jeśli adres docelowy używa HTTPS
-// 	})
-// );
-
-// Endpoint '/test' w twojej aplikacji serwerowej
-
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', 'https://nodejsclusters-158150-0.cloudclusters.net');
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-// 	res.header('Access-Control-Allow-Credentials', 'true');
-// 	next();
-//   });
-// const db = mysql.createConnection({
-// 	host: 'mysql-157993-0.cloudclusters.net',
-// 	port: 10039,
-// 	user: 'admin',
-// 	password: 'AgLEorrf',
-// 	database: 'products',
-// });
-// const db = mysql.createConnection({
-// 	host: 'host491364.hostido.net.pl',
-// 	// port: 21,
-// 	user: 'host491364_products',
-// 	password: 'Panpascal69',
-// 	database: 'host491364_products',
-// });
 const db = mysql.createConnection({
 	host: 'viaduct.proxy.rlwy.net',
 	port: 47341,
@@ -198,53 +75,6 @@ db.connect((err) => {
 	  console.log('Pomyślnie wykonano polecenie SET GLOBAL host_cache_size=0');
 	});
   });
-// app.get('/test', async (req, res) => {
-// 	try {
-// 		const testUrl = 'https://maksymalnytrener.pl/';
-// 		const response = await fetch(testUrl);
-// 		const data = await response.text();
-// 		console.log('Odpowiedź z serwera:', data);
-// 		res.send('Odpowiedź na zapytanie GET na /test');
-// 	} catch (error) {
-// 		console.error('Błąd podczas połączenia:', error);
-// 		res.status(500).send('Błąd podczas pobierania danych');
-// 	}
-// });
-
-// const testUrl = 'https://maksymalnytrener.pl/';
-
-// https
-// 	.get(testUrl, (response) => {
-// 		let data = '';
-// 		response.on('data', (chunk) => {
-// 			data += chunk;
-// 		});
-
-// 		response.on('end', () => {
-// 			console.log('Odpowiedź z serwera:', data);
-// 		});
-// 	})
-// 	.on('error', (error) => {
-// 		console.error('Błąd podczas połączenia:', error);
-// 	});
-
-// const url = 'https://maksymalnytrener.pl';
-
-// https
-// 	.get(url, (response) => {
-// 		let data = '';
-// 		response.on('data', (chunk) => {
-// 			data += chunk;
-// 		});
-
-// 		response.on('end', () => {
-// 			console.log('Odpowiedź z serwera:', data);
-// 		});
-// 	})
-// 	.on('error', (error) => {
-// 		console.error('Błąd podczas połączenia:', error);
-// 	});
-
 app.get('/getProducts/:username', (req, res) => {
 	const username = req.params.username;
 	const userToken = req.headers.authorization.split(' ')[1];
@@ -313,10 +143,7 @@ db.connect((err) => {
 	}
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-	console.log(`Serwer działa na porcie ${PORT}`);
-});
+
 
 app.get('/allTasks/:username', (req, res) => {
 	const username = req.params.username;
@@ -351,68 +178,8 @@ app.get('/allTasks/:username', (req, res) => {
 	}
 });
 
-// app.post('/register', (req, res) => {
-// 	const { username, password, email } = req.body;
 
-// 	const payload = {
-// 		username: username,
-// 		email: email,
-// 	};
-// 	const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
 
-// 	const sql = `INSERT INTO users (username, password, email, token) VALUES (?, ?, ?, ?)`;
-// 	db.query(sql, [username, password, email, token], (err, result) => {
-// 		if (err) {
-// 			console.error('Błąd podczas rejestracji użytkownika:', err);
-// 			return res
-// 				.status(500)
-// 				.json({ message: 'Błąd podczas rejestracji użytkownika.' });
-// 		}
-// 		console.log('Użytkownik został zarejestrowany:', { username, email });
-// 		res
-// 			.status(201)
-// 			.json({ message: 'Użytkownik został zarejestrowany.', token: token });
-// 	});
-// });
-
-// app.post('/login', (req, res) => {
-// 	const { username, password } = req.body;
-
-// 	const sql = `SELECT * FROM users WHERE username = ? AND password = ?`;
-// 	db.query(sql, [username, password], (err, results) => {
-// 		if (err) {
-// 			console.error('Błąd podczas logowania:', err);
-// 			return res.status(500).json({ message: 'Błąd logowania.' });
-// 		}
-
-// 		if (results.length === 0) {
-// 			return res.status(401).json({ message: 'Nieprawidłowe dane logowania.' });
-// 		}
-
-// 		const row = results[0];
-// 		const payload = {
-// 			username: row.username,
-// 			email: row.email,
-// 		};
-// 		const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-
-// 		const updateTokenQuery = `UPDATE users SET token = ? WHERE id = ?`;
-// 		db.query(updateTokenQuery, [token, row.id], (err, result) => {
-// 			if (err) {
-// 				console.error('Błąd podczas aktualizacji tokenu:', err);
-// 				return res.status(500).json({ message: 'Błąd logowania.' });
-// 			}
-
-// 			console.log('Użytkownik zalogowany:', {
-// 				username: row.username,
-// 				email: row.email,
-// 			});
-// 			res
-// 				.status(200)
-// 				.json({ message: 'Zalogowano użytkownika.', token: token });
-// 		});
-// 	});
-// });
 const crypto = require('crypto');
 const { create } = require('domain');
 
@@ -479,49 +246,7 @@ db.query(createProductsTableQuery, (err, result) => {
 		console.log('Tabela products utworzona pomyślnie.');
 	}
 });
-
-// app.post('/register', (req, res) => {
-// 	const { username, password, email } = req.body;
-
-// 	const token = jwt.sign({ username, email }, secretKey, { expiresIn: '1h' });
-
-// 	const sql = `INSERT INTO users (username, password, email, token) VALUES (?, ?, ?, ?)`;
-// 	db.run(sql, [username, password, email, token], (err) => {
-// 		if (err) {
-// 			console.error('Błąd podczas rejestracji użytkownika:', err);
-// 			return res
-// 				.status(500)
-// 				.json({ message: 'Błąd podczas rejestracji użytkownika.' });
-// 		}
-// 		console.log('Użytkownik został zarejestrowany:', { username, email });
-// 		res
-// 			.status(201)
-// 			.json({ message: 'Użytkownik został zarejestrowany.', token });
-// 	});
-// 	console.log('Received POST request:', req.body); // Logowanie danych żądania
-//   res.send('Response from server')
-// });
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-app.use(helmet());
-app.use(express.json());
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', allowedOrigin); // Zmodyfikuj na właściwy adres Twojego localhost
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-  
-//     if (req.method === 'OPTIONS') {
-//       res.sendStatus(200); // Odpowiedź 200 na żądania OPTIONS
-//     } else {
-//       next(); // Przejdź do kolejnego middleware'u dla innych rodzajów żądań
-//     }
-//   });
-//   app.use('/api', createProxyMiddleware({ 
-// 	target: process.env.REACT_APP_BACKEND_URL, 
-// 	changeOrigin: true,
-//   }));
-  app.post('/register', (req, res) => {
+app.post('/register', (req, res) => {
     const { username, password, email } = req.body;
   
     const token = jwt.sign({ username, email }, 'secretKey', { expiresIn: '1h' });
@@ -639,4 +364,12 @@ app.post('/addTask', (req, res) => {
 	});
 
 	res.status(201).json({ message: 'Zadania zostały dodane.' });
+});
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+	console.log(`Serwer działa na porcie ${PORT}`);
 });
