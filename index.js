0ad2e1473e11a44010f6018c8e3f8be43ebd5c2a
Max <syst38@gmail.com>
2023-12-28 18:49:33 +0100

    first commit

--- index.js
+++ index.js
@@ -641,7 +641,7 @@ app.post('/addTask', (req, res) => {
 });
 app.post('/updateTasks', (req, res) => {
     // let editedTasks = req.body.editedTasks; // Dane przesłane z przeglądarki
-	const { editedTask } = req.body;
+	const { s } = req.body;
     const userToken = req.headers.authorization;
 	console.log(req.body)
 	if (typeof editedTask !== 'undefined') {
